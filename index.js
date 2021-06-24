16a33f72758b313a15ca06ba4e1cd31c7029fb09
Shugeeth Babu <meetshugeeth@gmail.com>
2021-06-24 15:23:00 +0530

    Node MongoDB Example 1

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 const { MongoClient } = require('mongodb')
 const assert = require('assert')
+const dboper = require('./operations')
 
 const url = 'mongodb://127.0.0.1:27017/'
 const dbName = 'conFusion'
 
 const client = new MongoClient(url, { useUnifiedTopology: true })
 client.connect((err, client) => {
+
     assert.equal(err, null)
     console.log("Connected correctly to server")
-
     const db = client.db(dbName)
-    const collection = db.collection('dishes')
+    
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"}, 
+    'dishes', (result) => {
+        console.log("Insert Document:\n", result.ops)
+        dboper.findDocuments(db, 'dishes',
+        (docs) => {
+            console.log("Found Documents:\n", docs)
+            dboper.updateDocument(db, { name: "Vadonut" }, { description: "Updated Test" },
+            'dishes', (result) => {
+                console.log("Updated Document:\n", result.result)
+                // client.close()
 
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
-    (err, result) =>{
-        assert.equal(err, null)
-        console.log('After Insert:\n')
-        console.log(result.ops)
-
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err, null)
-            console.log('Found:\n')
-            console.log(docs)
-            // client.close()
-            db.dropCollection('dishes', (err, result) => {
-                assert.equal(err, null)
-
-                client.close()
+                dboper.removeDocuments(db, {name: 'Vadonut'},
+                'dishes', (result) => {
+                    console.log("Removed "+result.result.n+" Document.\n")
+                    db.dropCollection('dishes', (result) => {
+                        console.log("Dropped Collection: ", result)
+                        client.close()
+                    })
+                })
+                
             })
         })
     })
+
 })
